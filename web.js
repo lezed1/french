7cbb281c66492d996eb7fcc271209398a3488b3e
Zander Bolgar <zander@ZandBox.(none)>
2013-02-22 12:35:17 -0500

    Socket.io added

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
-var express = require('express');
+var app = require('express')()
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
 
-var app = express.createServer(express.logger());
+server.listen(80);
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+	console.log(data);
+    });
+});
+
+io.configure(function () { 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
 });
